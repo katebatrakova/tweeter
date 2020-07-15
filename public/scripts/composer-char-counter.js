92a628279c79e28fccbd285bc2196a8266105eb1
katebatrakova <kateryna.batrakova@gmail.com>
2020-07-14 20:54:43 -0400

    Create main container, tweets section, fixed navbar, dynamic counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+// file is solely responsible for this character counter//
+
+//runs a callback when the DOM is ready to be manipulated with jQuery
+$(document).ready(function () {
+  const textArea = $('#tweet-text');
+
+  textArea.on('keyup', function (event) {
+    //this refers to the element on which the event triggered
+    let inputLength = this.value.length;
+
+    // traverse up the DOM tree to find a parent
+    const parent = $(event.target).closest('form');
+    //go down to get the count and update its value 
+    let dynamicCounter = parent.find('.counter')[0].textContent = 140 - inputLength;
+    // target the counter
+    if (dynamicCounter < 0) {
+      $(".counter").css("color", "#FF0000	");
+    } else {
+      $(".counter").css("color", "#545149");
+    }
+  });
+});
--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -32,3 +32,87 @@ app.use("/tweets", tweetsRoutes);
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
+
+
+// ___<!-- LECTURE -->
+
+// Browser Object Model
+
+// console.log('inner Height', window.innerHeight)
+// console.log('inner Height', window.innerHeight)
+
+//DOM
+
+// catch a click, target an element that's gonna be clicked, detect the submit of the form event
+
+// const frm = document.querySelector('form');
+
+// // create listenning to an event. register an event.
+// frm.addEventListener('submit', function (event) {
+//   // prevent the submission of the form
+//   event.preventDefault();
+//   console.log("Form submitted!");
+//   console.log(event.target); //same is (this)
+//   //espect this to be the form element
+//   //extract the value of the input 
+//   // frm.elements.todo - give the input with that name
+//   //access the text content . this  - equivalent to frm;
+//   const actualTextContent = this.elements.todo.value;
+//   //create a new list item
+//   const newLi = document.createElement('li');
+//   newLi.textContent = actualTextContent + ' ';
+//   //target the parent 
+//   const todoList = document.querySelector('#todos')
+//   //create a buttob and add it to lis element
+//   const delButton = document.createElement('button');
+//   delButton.textContent = 'X';
+//   //add the button to the new list
+//   newLi.appendChild(delButton);
+//   //add event liostener to delet btn
+//   delButton.addEventListener('click', function (even) {
+//     //remove child from ul
+//     todoList.removeChild(newLi); //or this.parentElement;
+//   })
+//   //  append 
+//   todoList.appendChild(newLi);
+//   // reset  the text contect of the input test \
+//   this.elements.todo = "";
+// });
+
+// // _____
+// const petsArr = {
+//   name:
+//     type:
+//   age:
+// }
+
+// const createArticle = ((aninamlObj) => {
+//   const article = `
+//   // html goes inside, insert inside
+
+//     < h2 > ${ animalObj.name}</h2>
+//       ${ animalObj.type}
+//   ${ animalObj.age}
+
+//   `
+//   return article;
+// })
+
+
+// const renderArticles = animalsArr => {
+
+//   for (let animaObj of animals) {
+//     $('#pets-container').append(createArticle(petsArr[animaObj]));
+
+//   }
+// }
+
+// //ensuring that DOM is fully loaded before manipulating it
+// $(document).ready(function () {
+//   //append the articles to the section page
+//   $('#pets-container').append(createArticle(petsArr));
+//   renderArticles(petsArr);
+// })
+
+
