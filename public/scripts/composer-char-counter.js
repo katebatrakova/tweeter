f377e73c4ac12e76d264f6efd3c80b0c6762a73b
katebatrakova <kateryna.batrakova@gmail.com>
2020-07-16 17:49:42 -0400

    Update the counter to properly reset upon clicking on Submit and not update if error

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -12,11 +12,24 @@ $(document).ready(function () {
     const parent = $(event.target).closest('form');
     //go down to get the count and update its value 
     let dynamicCounter = parent.find('.counter')[0].textContent = 140 - inputLength;
+    //reset the counter after submitting
+    $('#submitbtn').click(function () {
+      $('.counter').text('140');
+    })
+    console.log(dynamicCounter, 'dynamicCounter')
     // target the counter
     if (dynamicCounter < 0) {
       $(".counter").css("color", "#FF0000	");
+      $('#submitbtn').click(function () {
+        //do not reset the counter upon submission
+        $('.counter').text(dynamicCounter);
+      })
     } else {
       $(".counter").css("color", "#545149");
     }
+
   });
+
+
+
 });
